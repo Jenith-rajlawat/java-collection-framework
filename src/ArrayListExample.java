3c6f4202ca5323c68a3e70587a5cfb5872281386
Jenith Rajlawat <jenithrajlawat@gmail.com>
2023-12-29 23:59:49 +0545

    Added comments for better understanding

--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -4,16 +4,16 @@ import java.util.List;
 public class ArrayListExample {
 
 	public static void main(String[] args) {
-		List<String> al=new ArrayList<String>();
+		List<String> al=new ArrayList<String>();//Constructs an empty list with an initial capacity of ten.
 		
 		al.add("Jenith");
 		al.add("Jesika");
 		
-		Iterator itr=al.iterator();
+		Iterator itr=al.iterator(); //Returns an iterator over the elements in this list in proper sequence.
 			while(itr.hasNext()) {
-				System.out.println(itr.next());
+				System.out.println(itr.next()); //Returns the next element in the iteration.
 			}
-		
+		//Returns true if the iteration has more elements.(In other words, returns true if next wouldreturn an element rather than throwing an exception.)
 		
 	}
 	
